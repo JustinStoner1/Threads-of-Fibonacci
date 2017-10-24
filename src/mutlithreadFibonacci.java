9a2d620d4a1a7be26114e2a2698e696f70539e54
JustinStoner1 <31672859+JustinStoner1@users.noreply.github.com>
2017-10-24 12:09:00 -0600

    Add files via upload

--- src/mutlithreadFibonacci.java
+++ src/mutlithreadFibonacci.java
@@ -3,32 +3,47 @@ public class mutlithreadFibonacci
 
   public static void main(String args[])
   {
-    Worker workerOne = new Worker("One");
+    Worker workerOne = new Worker("FibThread One");
     Thread threadOne = new Thread(workerOne);
     threadOne.start();
 
-    Worker workerTwo = new Worker("Two");
+    Worker workerTwo = new Worker("FibThread Two");
     Thread threadTwo = new Thread(workerTwo);
     threadTwo.start();
 
+    int iterations = 0;
     while (threadOne.isAlive() && threadTwo.isAlive())
     {
       System.out.println(workerOne);
       System.out.println(workerTwo);
+      iterations++;
+      if (iterations >= 10)
+      {
+        workerOne.die();
+        workerTwo.die();
+      }
+      try
+      {
+        Thread.sleep(2000);
+      }
+      catch (Exception error)
+      {
+        System.out.println("Thread had too much caffeine, could not sleep");
+      }
     }
-
+    System.out.println("Program Exit");
+    System.exit(1);
   }
 }
 
 class Worker implements Runnable
 {
-  String oshaGuideLines;
-
   private final String NAME;
   private long step = 0;
   private long y = 1;
   private long x = 1;
-  private long z = x + y;
+  private long z;
+  private volatile boolean shouldBeAlive = true;
 
   public Worker(String NAME)
   {
@@ -40,7 +55,13 @@ class Worker implements Runnable
   {
     while (true)
     {
-      nextStep();
+      if (shouldBeAlive) nextStep();
+      else
+      {
+        System.out.println("Thread" + NAME + " has died");
+        break;
+        //System.exit(1);
+      }
     }
   }
 
@@ -60,8 +81,13 @@ class Worker implements Runnable
     }
   }
 
+  public void die()
+  {
+    shouldBeAlive = false;
+  }
+
   public synchronized String toString()
   {
-    return NAME + " z: "+z+" x: "+x+" y: "+y;
+    return NAME + " Step#: " + step + " z: "+z+" x: "+x+" y: "+y;
   }
 }
